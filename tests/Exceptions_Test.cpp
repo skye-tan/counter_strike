87d9f2fa7ffdeaed4120c8d7194a0ad79ae564e1
Amir Hossein Ghadiri <amirpax32@gmail.com>
2023-08-20 17:40:47 +0330

    feat: added the unit tests for different types of dead player exceptions.

--- tests/Exceptions_Test.cpp
+++ tests/Exceptions_Test.cpp
@@ -4,6 +4,9 @@
 #include "Full_Team_Exception.h"
 #include "Friendly_Fire_Exception.h"
 #include "Dead_Player_Exception.h"
+#include "Dead_Buyer_Exception.h"
+#include "Dead_Attacker_Exception.h"
+#include "Dead_Attacked_Exception.h"
 #include "gtest/gtest.h"
 
 TEST(Exceptions, Custom) {
@@ -44,9 +47,36 @@ TEST(Exceptions, Friendly_Fire) {
 
 TEST(Exceptions, Dead_Player) {
 
-    Dead_Player_Exception test_exception("dead player");
+    Dead_Player_Exception test_exception("player is dead");
 
-    EXPECT_EQ(test_exception.what(), "dead player");
+    EXPECT_EQ(test_exception.what(), "player is dead");
+    EXPECT_EQ(0, 0) << test_exception;
+
+}
+
+TEST(Exceptions, Dead_Buyer) {
+
+    Dead_Buyer_Exception test_exception;
+
+    EXPECT_EQ(test_exception.what(), "deads can not buy");
+    EXPECT_EQ(0, 0) << test_exception;
+
+}
+
+TEST(Exceptions, Dead_Attacker) {
+
+    Dead_Attacker_Exception test_exception;
+
+    EXPECT_EQ(test_exception.what(), "attacker is dead");
+    EXPECT_EQ(0, 0) << test_exception;
+
+}
+
+TEST(Exceptions, Dead_Attacked) {
+
+    Dead_Attacked_Exception test_exception;
+
+    EXPECT_EQ(test_exception.what(), "attacked is dead");
     EXPECT_EQ(0, 0) << test_exception;
 
 }
