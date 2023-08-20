1666476986a3de5e90d5a1421534e04dba3a1b75
Amir Hossein Ghadiri <amirpax32@gmail.com>
2023-08-20 13:28:44 +0330

    feat: added the Heavy_Gun.cpp which contains the implementation for the Heavy_Gun class.

---
+++ src/Heavy_Gun.cpp
@@ -0,0 +1,5 @@
+
+#include "Heavy_Gun.h"
+
+Heavy_Gun::Heavy_Gun(const std::string input_name, const int input_damage, const int input_reward) 
+    : Weapon(input_name, input_damage, input_reward) {}
