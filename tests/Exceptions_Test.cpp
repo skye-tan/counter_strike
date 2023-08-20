f9b3abd16cb67878fad069b8a566c6be4ef79161
Amir Hossein Ghadiri <amirpax32@gmail.com>
2023-08-20 17:19:34 +0330

    feat: created unit test for the Dead_Player_Exception class.

--- tests/Exceptions_Test.cpp
+++ tests/Exceptions_Test.cpp
@@ -3,6 +3,7 @@
 #include "Insufficient_Money_Exception.h"
 #include "Full_Team_Exception.h"
 #include "Friendly_Fire_Exception.h"
+#include "Dead_Player_Exception.h"
 #include "gtest/gtest.h"
 
 TEST(Exceptions, Custom) {
@@ -37,6 +38,15 @@ TEST(Exceptions, Friendly_Fire) {
     Friendly_Fire_Exception test_exception;
 
     EXPECT_EQ(test_exception.what(), "friendly fire");
+    EXPECT_EQ(0, 0) << test_exception;
+
+}
+
+TEST(Exceptions, Dead_Player) {
+
+    Dead_Player_Exception test_exception("dead player");
+
+    EXPECT_EQ(test_exception.what(), "dead player");
     EXPECT_EQ(0, 0) << test_exception;
 
 }
