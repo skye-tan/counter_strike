a86a544745cffcf9821ef3e723529c998c943592
Amir Hossein Ghadiri <amirpax32@gmail.com>
2023-08-20 13:27:24 +0330

    feat: added the Pistol.h header containing its attributes and methods

---
+++ include/Pistol.h
@@ -0,0 +1,30 @@
+
+#include <string>
+#include "Weapon.h"
+
+#ifndef PISTOL_H
+#define PISTOL_H
+
+/**
+ * @brief Store the main attributes of a pistol
+ * 
+ *   Name, damage and the recived reward which are the common attributes
+ * of any kind of pistol are stored here.
+ * 
+ */
+class Pistol : public Weapon {
+
+public:
+
+    /**
+     * @brief Construct a new Pistol object
+     * 
+     * @param input_name Weapon's name
+     * @param input_damage Damage dealt by this weapon
+     * @param input_reward Recived reward for killing an enemy with this weapon
+     */
+    Pistol(const std::string input_name = "", const int input_damage = 0, const int input_reward = 0);
+
+};
+
+#endif
