3627afae745afd7958e8300d229ae5073bf4660e
Amir Hossein Ghadiri <amirpax32@gmail.com>
2023-08-20 18:35:30 +0330

    feat: added he Duplicate_UserName_Exception.h header.

---
+++ include/Duplicate_UserName_Exception.h
@@ -0,0 +1,24 @@
+
+#include "Duplicate_Attribute_Exception.h"
+
+#ifndef DUPLICATE_USERNAME_EXCEPTION_H
+#define DUPLICATE_USERNAME_EXCEPTION_H
+
+/**
+ * @brief Exception for when a duplicate attribute has been given
+ * 
+ */
+class Duplicate_UserName_Exception : public Duplicate_Attribute_Exception {
+
+public:
+
+    /**
+     * @brief Construct a new Duplicate_UserName_Exception object
+     * 
+     * @param input_msg the excpetion's message
+     */
+    Duplicate_UserName_Exception();
+
+};
+
+#endif
