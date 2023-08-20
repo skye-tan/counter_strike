5eaf35af4c85f285f69f252833f262392d3337c3
Amir Hossein Ghadiri <amirpax32@gmail.com>
2023-08-20 18:36:52 +0330

    feat: added the implementation for the Duplicate_WeaponType_Exception class.

---
+++ src/Duplicate_WeaponType_Exception.cpp
@@ -0,0 +1,5 @@
+
+#include "Duplicate_WeaponType_Exception.h"
+
+Duplicate_WeaponType_Exception::Duplicate_WeaponType_Exception(std::string input_msg) :
+    Duplicate_Attribute_Exception(input_msg) {}
