4aa2e3fad8d30f2c6b3d631189248953c3751973
Amir Hossein Ghadiri <amirpax32@gmail.com>
2023-08-20 13:29:04 +0330

    feat: added the Knife.h header containing its attributes and methods

---
+++ include/Knife.h
@@ -0,0 +1,25 @@
+
+#include <string>
+#include "Weapon.h"
+
+#ifndef KNIFE_H
+#define KNIFE_H
+
+/**
+ * @brief Store the main attributes of a knife
+ * 
+ *   Name, damage and the recived reward which are the common attributes
+ * of any kind of knife are stored here which will be initialized with constant
+ * values in the constructor.
+ * 
+ */
+class Knife : public Weapon {
+
+    /**
+     * @brief Construct a new Pistol object
+     */
+    Knife();
+    
+};
+
+#endif
