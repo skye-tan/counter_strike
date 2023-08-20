62a8190dbddd32264e743fcf1e68d9b81f2890f7
Amir Hossein Ghadiri <amirpax32@gmail.com>
2023-08-20 17:13:26 +0330

    feat: created unit test for the Friendly_Fire_Exception class.

--- tests/Exceptions_Test.cpp
+++ tests/Exceptions_Test.cpp
@@ -2,6 +2,7 @@
 #include "Custom_Exception.h"
 #include "Insufficient_Money_Exception.h"
 #include "Full_Team_Exception.h"
+#include "Friendly_Fire_Exception.h"
 #include "gtest/gtest.h"
 
 TEST(Exceptions, Custom) {
@@ -27,6 +28,15 @@ TEST(Exceptions, Full_Team) {
     Full_Team_Exception test_exception;
 
     EXPECT_EQ(test_exception.what(), "this team is full");
+    EXPECT_EQ(0, 0) << test_exception;
+
+}
+
+TEST(Exceptions, Friendly_Fire) {
+
+    Friendly_Fire_Exception test_exception;
+
+    EXPECT_EQ(test_exception.what(), "friendly fire");
     EXPECT_EQ(0, 0) << test_exception;
 
 }
