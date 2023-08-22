3dd98f88b845d38db49ef3aa967a2255c45067c3
Amir Hossein Ghadiri <amirpax32@gmail.com>
2023-08-22 22:58:12 +0330

    fix: updated the unit test for the Weapon_Bundle class.

--- tests/Weapon_Bundle_Test.cpp
+++ tests/Weapon_Bundle_Test.cpp
@@ -6,40 +6,77 @@
 TEST(Weapon_Bundle_Class, pistol_Atrribute) {
 
     Weapon_Bundle test_bundle;
-    Pistol test_pistol("Revolver", 51, 150);
+    Pistol test_pistol("Revolver", 600, 51, 150);
 
     try {
         test_bundle.get_pistol();
     }
     catch (Invalid_GunType_Exception e) {
-        std::cout << "exception thrown!" << std::endl;
+        std::cout << e << std::endl;
         EXPECT_EQ(e.what(), "no such gun");
     }
 
-    test_bundle.change_pistol(test_pistol);
+    test_bundle.add_pistol(test_pistol);
     EXPECT_EQ(test_bundle.get_pistol().get_name(), "Revolver");
+    EXPECT_EQ(test_bundle.get_pistol().get_price(), 600);
     EXPECT_EQ(test_bundle.get_pistol().get_damage(), 51);
     EXPECT_EQ(test_bundle.get_pistol().get_reward(), 150);
+
+    try {
+        test_bundle.add_pistol(test_pistol);
+    }
+    catch (Duplicate_Pistol_Exception e) {
+        std::cout << e << std::endl;
+        EXPECT_EQ(e.what(), "you have a pistol");
+    }
+    
+    test_bundle.remove_pistol();
+    try {
+        test_bundle.get_pistol();
+    }
+    catch (Invalid_GunType_Exception e) {
+        std::cout << e << std::endl;
+        EXPECT_EQ(e.what(), "no such gun");
+    }
 
 }
 
 TEST(Weapon_Bundle_Class, heavy_gun_Atrribute) {
 
     Weapon_Bundle test_bundle;
-    Heavy_Gun test_heavy_gun("AK", 31, 100);
+    Heavy_Gun test_heavy_gun("AK", 2700, 31, 100);
 
     try {
         test_bundle.get_heavy_gun();
     }
     catch (Invalid_GunType_Exception e) {
-        std::cout << "exception thrown!" << std::endl;
+        std::cout << e << std::endl;
         EXPECT_EQ(e.what(), "no such gun");
     }
 
-    test_bundle.change_heavy_gun(test_heavy_gun);
+    test_bundle.add_heavy_gun(test_heavy_gun);
     EXPECT_EQ(test_bundle.get_heavy_gun().get_name(), "AK");
+    EXPECT_EQ(test_bundle.get_heavy_gun().get_price(), 2700);
     EXPECT_EQ(test_bundle.get_heavy_gun().get_damage(), 31);
     EXPECT_EQ(test_bundle.get_heavy_gun().get_reward(), 100);
+
+    try {
+        test_bundle.add_heavy_gun(test_heavy_gun);
+    }
+    catch (Duplicate_Heavy_Gun_Exception e) {
+        std::cout << e << std::endl;
+        EXPECT_EQ(e.what(), "you have a heavy");
+    }
+
+    test_bundle.remove_heavy_gun();
+
+        try {
+        test_bundle.get_heavy_gun();
+    }
+    catch (Invalid_GunType_Exception e) {
+        std::cout << e << std::endl;
+        EXPECT_EQ(e.what(), "no such gun");
+    }
 
 }
 
