753192e90c2b38e956a2998e28984aa594eb97c0
Amir Hossein Ghadiri <amirpax32@gmail.com>
2023-08-22 22:40:13 +0330

    fix: updated the unit test for the Heavy_Gun class.

--- tests/Heavy_Gun_Test.cpp
+++ tests/Heavy_Gun_Test.cpp
@@ -4,9 +4,10 @@
 
 TEST(Heavy_Gun_Class, Getters) {
 
-    Heavy_Gun test_heavy_gun("AK", 31, 100);
+    Heavy_Gun test_heavy_gun("AK", 2700, 31, 100);
 
     EXPECT_EQ(test_heavy_gun.get_name(), "AK");
+    EXPECT_EQ(test_heavy_gun.get_price(), 2700);
     EXPECT_EQ(test_heavy_gun.get_damage(), 31);
     EXPECT_EQ(test_heavy_gun.get_reward(), 100);
 
