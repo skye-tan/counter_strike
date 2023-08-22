d6bde3a4c59dd16bccb27f05517fc2feb5cb8ece
Amir Hossein Ghadiri <amirpax32@gmail.com>
2023-08-22 22:36:32 +0330

    fix: updated the properties of the Pistol class.

--- src/Pistol.cpp
+++ src/Pistol.cpp
@@ -1,5 +1,5 @@
 
 #include "Pistol.h"
 
-Pistol::Pistol(const std::string input_name, const int input_damage, const int input_reward) 
-    : Weapon(input_name, input_damage, input_reward) {}
+Pistol::Pistol(const std::string input_name, const int input_price, const int input_damage, const int input_reward) 
+    : Weapon(input_name, input_price, input_damage, input_reward) {}
