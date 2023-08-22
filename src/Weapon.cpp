0899420b3421c01cc19675b6c8c0a6a960d35d76
Amir Hossein Ghadiri <amirpax32@gmail.com>
2023-08-22 22:35:32 +0330

    fix: updated the properties of the Weapon class.

--- src/Weapon.cpp
+++ src/Weapon.cpp
@@ -1,8 +1,8 @@
 
 #include "Weapon.h"
 
-Weapon::Weapon(const std::string input_name, const int input_damage, const int input_reward) 
-    : name(input_name), damage(input_damage), reward(input_reward) {}
+Weapon::Weapon(const std::string input_name, const int input_price, const int input_damage, const int input_reward) 
+    : name(input_name), price(input_price), damage(input_damage), reward(input_reward) {}
 
 std::string Weapon::get_name() const {
     return name;
@@ -13,5 +13,9 @@ int Weapon::get_damage() const {
 }
 
 int Weapon::get_reward() const {
-    return  reward;
+    return reward;
+}
+
+int Weapon::get_price() const {
+    return price;
 }
