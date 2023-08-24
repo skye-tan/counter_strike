18b61ee8b417a9ac4b4f7ae7d641c1910c4ac615
Amir Hossein Ghadiri <amirpax32@gmail.com>
2023-08-24 15:19:33 +0330

    feat: added a new test for the Timer class's operators.

--- tests/Timer_Test.cpp
+++ tests/Timer_Test.cpp
@@ -11,3 +11,14 @@ TEST(Timer_Class, Getters) {
     EXPECT_EQ(test_timer.get_milliseconds(), 178);
 
 }
+
+TEST(Timer_Class, Operators) {
+
+    Timer test_timer_1(2, 25, 178);
+    Timer test_timer_2(1, 25, 178);
+
+    EXPECT_EQ(test_timer_1 > test_timer_2, true);
+    EXPECT_EQ(test_timer_1 > test_timer_1, false);
+    EXPECT_EQ(test_timer_1 >= test_timer_1, true);
+
+}
